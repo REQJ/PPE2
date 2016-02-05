a0d7bf6d4ea65044142654a4bda725254c1a5267
Jérôme <Jrme@DESKTOP-P54V8MS>
2016-02-05 10:35:33 +0100

    jr

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -2,11 +2,12 @@ seb
     
     :;mlkl
         ,k,;:,
-            <?php
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+lmùmùmùlmùl4kklmkl
+            ,kjnknk
+        jlkjhlkhl
