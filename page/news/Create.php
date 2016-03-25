69379c8235c17d3b39140446d64348ba20b9b834
Jérôme <Jrme@DESKTOP-P54V8MS>
2016-03-25 11:07:39 +0100

    modification de dossier

--- page/news/Create.php
+++
@@ -1,5 +0,0 @@
-<form action="index.php?routeur=news&action=store" method="post">
-	Titre : <input type="text" name="titre">
-	Description : 	<textarea name="description"></textarea>
-	<input type="submit">
-</form>
